85585c4e63f7f112b7d66161589d1100cd6aa6a6
WarriUrbosa23 <thekiddojac@gmail.com>
2025-07-24 14:14:21 -0700

    Leila made a full comparison :3

---
+++ whale proteins/full-compasrison.py
@@ -0,0 +1,24 @@
+def compare(x, y):
+    counts = 0
+    for i in range(len(x)):
+        if x[i] != y[i]:
+            counts += 1
+            print(i, x[i], y[i])
+    print(counts)
+
+    return counts
+
+f = open("whale.aln")
+lines=f.readlines()
+
+whales=["","",""]
+for i in range(0, 3):
+	for j in range(10*i+1,10*i+9):
+		whales[i] += lines[j].rstrip('\n')
+#print("beluga\n",whales[0],"\ndolphin\n",whales[1],"\norca\n",whales[2])
+print("beluga vs dolphin")
+compare(whales[0], whales[1])
+print("dolphin vs orca")
+compare(whales[1],whales[2])
+print("orca vs beluga")
+compare(whales[2],whales[0])
